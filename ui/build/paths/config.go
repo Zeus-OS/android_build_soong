3f6640a6055f354debabc56d5048e72ab8efb170
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-07-27 21:58:15 +0200

    Allow arm-linux-androidkernel-ld

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -123,6 +123,7 @@ var Configuration = map[string]PathConfig{
 	"ping":         Allowed,
 	"curl":         Allowed,
 	"arm-linux-androidkernel-as":         Allowed,
+	"arm-linux-androidkernel-ld":         Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
